1fc86472d20da0fca1dc2577fc3079e276eabe41
bajracharya-kshitiz01 <bajracharyak01@gmail.com>
2025-12-28 13:10:04 +0545

    Fixed some

--- fourth.cpp
+++ fourth.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void getdata(){
   int x,y;
-  cout<<"Enter the first number: "<<endl;
+    cout<<"Enter the first number: "<<endl;
   cin>>x;
   cout<<"Enter the Second number: "<<endl;
   cin>>y;
+
+}
+int main(){
+  getdata();
+  return 0;
 }
